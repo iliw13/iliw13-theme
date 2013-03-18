10029e41e43dd5b314eed2df37ade8e8ad2f5a24
conatus <alex@recordsonribs.com>
2013-03-18 23:08:36 +0000

    Basic page template

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<div class="row-fluid">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="row-fluid">
+					<div class="span12"><h1><?php the_title(); ?></h1></div>
+				</div>
+</div>
+<div class="row-fluid">
+				<div class="span10"><?php the_content(); ?></div>
+			<?php endwhile; ?>
+		<?php else : ?>
+			No results!
+		<?php endif; ?>
+				<div class="span2">
+					<h4>Join the debate</h4>
+					<p>
+						<ul class="unstyled">
+							<li><a href="http://twitter.com/iliw13">@iliw13</a></li>
+							<li><a href="https://twitter.com/search?q=%23iliw13">#iliw13</a></li>
+						</ul>
+					</p>		
+				</div>
+</div>
+<hr>
+<?php get_footer(); ?>
